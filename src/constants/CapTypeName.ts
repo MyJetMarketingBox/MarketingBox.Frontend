9a55933e3cbe269b0e127e47c362680033832e52
Ruslan Goroshko <ashonetak@gmail.com>
2022-06-27 21:12:09 +0300

    TRFMW-715

--- src/constants/CapTypeName.ts
+++ src/constants/CapTypeName.ts
@@ -4,5 +4,5 @@ export const CapTypeName = {
   [CapTypeEnum.Clicks]: "Clicks",
   [CapTypeEnum.Ftds]: "Ftds",
   [CapTypeEnum.Impressions]: "Impressions",
-  [CapTypeEnum.Lead]: "Lead",
+  [CapTypeEnum.Lead]: "Leads",
 };
